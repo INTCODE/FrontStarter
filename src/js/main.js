bfe11ae3eaa78ed520a413481c6c1b8f42731f7b
bartektalar <bartek.talar@gmail.com>
2018-06-20 12:58:14 +0200

    price

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,60 @@
-// QUICK SEARCH JS
+// COLORPICKER
 //-------------------------------------------------------
-$('#search_product input').keyup(function () {
-    var container = $('.item-container'),
-        q = $(this).val();
-        // AJAX HERE!
-        // SPRAWDZ DŁUGOSC if > 0 esle BRAK WYNIKOW
-        // SUKCES .html() RESULTS
-        // container.append(q);
-});
-console.log(
-    "asdsds"
-);
-
-
 function setTextColor(picker) {
     document.getElementsByTagName('body')[0].style.color = '#' + picker.toString()
 }
+
+
+// Price Calc
+//-------------------------------------------------------
+
+var cenaNetto = $('#pricing #proce_netto'),
+    cenaBrutto = $('#pricing #proce_brutto'),
+    podatek = $('#pricing #tax');
+
+
+// Netto --> Brutto
+//---------------------------------------------------------------
+$(cenaNetto).on('keyup',function () {
+
+    if( $(this).val().length > 0 ) {
+        var netto = parseInt($(cenaNetto).val());
+
+        if( $(podatek).val().length > 0 ) {
+           var  tax = $(podatek).val();
+        }else{
+            var tax = 23; // Domyślnie ustawiam 23
+        }
+        var brutto = netto + (netto * (tax/100));
+        cenaBrutto.val(brutto.toFixed(2));
+    }else{
+        cenaNetto.val('');
+        cenaBrutto.val('');
+    }
+
+});
+
+// Brutto --> Netto
+//--------------------------------------------------------------
+$(cenaBrutto).on('keyup',function () {
+
+    if( $(this).val().length > 0 ) {
+        var brutto = parseInt($(cenaBrutto).val());
+
+
+        //oblicz wysokość vatu.
+
+
+        if( $(podatek).val().length > 0 ) {
+            var  tax = $(podatek).val();
+        }else{
+            var tax = 23; // Domyślnie ustawiam 23
+        }
+        var netto = (brutto*100)/(100+100*(tax/100)) ;
+        cenaNetto.val(netto.toFixed(2));
+    }else{
+        cenaNetto.val('');
+        cenaBrutto.val('');
+    }
+});
+
